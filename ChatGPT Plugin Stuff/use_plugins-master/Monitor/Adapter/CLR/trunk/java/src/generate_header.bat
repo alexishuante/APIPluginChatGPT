63512d068db78beb6487871dda70a00aa790c3eb
alexishuante <140995548+alexishuante@users.noreply.github.com>
2024-12-31 23:39:04 -0600

    Delete ChatGPT Plugin Stuff directory

--- ChatGPT Plugin Stuff/use_plugins-master/Monitor/Adapter/CLR/trunk/java/src/generate_header.bat
+++
@@ -1,7 +0,0 @@
-javah -jni -o CLRAdapter.h org.tzi.use.monitor.adapter.clr.CLRAdapter
-
-copy CLRAdapter.h C:\Users\Daniel\Git\CLR-Adapter\Src\CLRAdapter\CLRAdapter.h
-
-copy CLRAdapter.h C:\Users\Daniel\Svn\dbs\Monitor\Adapter\CLR\trunk\dotnet\Src\CLRAdapter\CLRAdapter.h
-
-pause
